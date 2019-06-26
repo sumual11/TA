e81c9e589d1f632e8f6d4f3b193f7287f8428ca6
sumual11 <m.adam91@gmail.com>
2019-06-26 16:32:19 +0700

    fix file asset

--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -8,21 +8,21 @@
   <meta name="description" content="">
   <meta name="author" content="">
 
-  <title>Creative - Start Bootstrap Theme</title>
+  <title>Jakarta Islamic Center - Pusat Peradaban Islam </title>
 
   <!-- Font Awesome Icons -->
-  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  
+<link href="{{asset('vendor/fontawesome-free/css/all.min.css')}}" rel="stylesheet" type="text/css">
 
   <!-- Google Fonts -->
   <link href="https://fonts.googleapis.com/css?family=Merriweather+Sans:400,700" rel="stylesheet">
   <link href='https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic' rel='stylesheet' type='text/css'>
 
   <!-- Plugin CSS -->
-  <link href="vendor/magnific-popup/magnific-popup.css" rel="stylesheet">
+  <link href="{{ asset('vendor/magnific-popup/magnific-popup.css') }}" rel="stylesheet">
 
   <!-- Theme CSS - Includes Bootstrap -->
-  <link href="css/creative.css" rel="stylesheet">
-
+  <link href="{{ asset('css/creative.css') }}" rel="stylesheet">
 </head>
 
 <body id="page-top">
@@ -33,23 +33,21 @@
 @yield('header')
 <!-- Put content here --> 
 @yield('content')
-<!-- Download Section -->
-@yield('download')
 <!-- Contact Section -->
 @include('frontend.items.contact')
 <!-- Footer -->
 @include('frontend.items.footer')
 
 <!-- Bootstrap core JavaScript -->
-  <script src="vendor/jquery/jquery.min.js"></script>
-  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="{{ asset('vendor/jquery/jquery.min.js')}}"></script>
+  <script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
 
   <!-- Plugin JavaScript -->
-  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
-  <script src="vendor/magnific-popup/jquery.magnific-popup.min.js"></script>
+  <script src="{{ asset('vendor/jquery-easing/jquery.easing.min.js')}}"></script>
+  <script src="{{ asset('vendor/magnific-popup/jquery.magnific-popup.min.js')}}"></script>
 
   <!-- Custom scripts for this template -->
-  <script src="js/creative.min.js"></script>
+  <script src="{{ asset('js/creative.min.js')}}"></script>
 
 </body>
 
