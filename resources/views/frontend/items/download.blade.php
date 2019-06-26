a9d9ca799e7e08e331102f1b9e9cf26c2524c60a
sumual11 <m.adam91@gmail.com>
2019-06-26 16:39:05 +0700

    hapus file view download

--- resources/views/frontend/items/download.blade.php
+++
@@ -1,6 +0,0 @@
-<section class="page-section-big bg-white text-dark">
-    <div class="container container-big text-center">
-      <h2 class="mb-4">Free Download at Start Bootstrap!</h2>
-      <a class="btn btn-light btn-xl sr-button" href="https://startbootstrap.com/template-overviews/creative/">Download Now!</a>
-    </div>
-  </section>
